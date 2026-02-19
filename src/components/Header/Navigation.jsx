ba108f4f83298187d2cc9ccfbe8ffca6be478dec
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-02-20 02:24:24 +0300

    Navigation component

---
+++ src/components/Header/Navigation.jsx
@@ -0,0 +1,20 @@
+import { Search as SearchIcon } from 'lucide-react';
+
+function Navigation({ children, onToggle }) {
+  return (
+    <div className='flex-1 relative'>
+      <ul className='text-2xl h-full bg-amber-200 max-w-3xs p-4 space-y-2'>
+        <li>Home</li>
+        <li className='flex gap-2 w-fit cursor-pointer' onClick={onToggle}>
+          Search
+          <button className='cursor-pointer'>
+            <SearchIcon></SearchIcon>
+          </button>
+        </li>
+      </ul>
+      {children}
+    </div>
+  );
+}
+
+export default Navigation;
