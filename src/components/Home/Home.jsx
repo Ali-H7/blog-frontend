55586a622fa9c1d44bf129cf5d2b632cbccb83ec
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-03-02 03:09:18 +0300

    Show skeleton when fetching posts

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import BlogCard from './BlogCard';
+import Skeleton from 'react-loading-skeleton';
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+
   useEffect(() => {
     const API = import.meta.env.VITE_API;
     async function fetchData() {
@@ -12,11 +15,24 @@ function Home() {
         setPosts(data.posts);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsFetching(false);
       }
     }
 
     fetchData();
   }, []);
+
+  if (isFetching) {
+    return (
+      <div className='p-8 flex flex-col gap-6'>
+        {new Array(5).fill({}).map((post, i) => (
+          <Skeleton key={i} count={5} style={{ marginBottom: '0.5rem' }} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className='p-8 flex flex-col gap-6'>
       {posts.map((post) => (
