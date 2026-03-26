f85bbf48b2702b30858d34cd99dea76aebfee68c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-03-26 07:46:18 +0300

    Fix esc closing bug
    when used escape previously closed state won't sync

--- src/components/shared/Dialog.jsx
+++ src/components/shared/Dialog.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-function Dialog({ position = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2', dialogStatus, children }) {
+function Dialog({ position = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2', dialogStatus, onClose, children }) {
   const dialogRef = useRef(null);
 
   useEffect(() => {
@@ -16,7 +16,7 @@ function Dialog({ position = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
     dialog.showModal();
   }, [dialogStatus]);
   return (
-    <dialog className='relative bg-transparent backdrop:bg-black/65' ref={dialogRef}>
+    <dialog className='relative bg-transparent backdrop:bg-black/65' ref={dialogRef} onClose={onClose}>
       <div className={` ${position} fixed flex w-full justify-center p-4`}>{children}</div>
     </dialog>
   );
