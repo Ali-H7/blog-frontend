40cb283b6245be83198924f90d32d647632d7f9c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-03-30 10:14:42 +0300

    Alert component

---
+++ src/components/shared/Alert.jsx
@@ -0,0 +1,25 @@
+import Dialog from './Dialog';
+import { CircleX as ErrorIcon, X as CloseIcon } from 'lucide-react';
+
+function Alert({
+  position = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+  dialogStatus,
+  onClose,
+  disableEscKey = false,
+}) {
+  const dialogProps = { position, dialogStatus, onClose, disableEscKey };
+  const alertText = dialogStatus ?? '';
+  return (
+    <Dialog {...dialogProps}>
+      <div className='bg-papaya_whip-400 relative flex items-center gap-4 rounded-md p-4'>
+        <CloseIcon className='absolute top-2 right-4 hover:cursor-pointer' onClick={onClose} />
+        <ErrorIcon className='shrink-0' size={48} />
+        <div>
+          <h1 className='font-bold'>Error!</h1>
+          <p>{alertText}</p>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+export default Alert;
