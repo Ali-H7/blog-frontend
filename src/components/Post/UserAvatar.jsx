dcad462f5c589ba7311c086ce5920cdf7173fa0e
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-04-12 22:44:21 +0300

    Give fixed colors for user UserAvatar
    this will ensure the user always have the same avatar

--- src/components/Post/UserAvatar.jsx
+++ src/components/Post/UserAvatar.jsx
@@ -1,6 +1,20 @@
-function UserAvatar({ letter }) {
+function UserAvatar({ name }) {
   const COLORS = ['#ef476f', '#ffd166', '#06d6a0', '#118ab2'];
-  const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+  const letter = name[0];
+
+  function pickColor() {
+    if (name === 'Guest') {
+      return '#808080';
+    } else {
+      const num = name.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+      const i = num % COLORS.length;
+      if (COLORS[i] === undefined) return '#808080';
+      return COLORS[i];
+    }
+  }
+
+  const color = pickColor();
+
   return (
     <div
       className='flex h-12.5 w-12.5 items-center justify-center rounded-md text-3xl font-bold text-white text-shadow-2xs text-shadow-black'
