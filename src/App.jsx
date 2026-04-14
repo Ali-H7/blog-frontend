18d467430b025e111788d8df018ac1c361f8cf63
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-04-14 04:54:28 +0300

    Add login global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { Outlet } from 'react-router';
 import Layout from './components/Layout';
+import { createContext, useContext, useState } from 'react';
+import getLoggedUser from './helpers/getLoggedUser';
+
+export const AuthContext = createContext();
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
 function App() {
+  const loggedUser = getLoggedUser();
+  const [user, setUser] = useState(loggedUser);
+
+  function login(userData) {
+    localStorage.setItem('userData', JSON.stringify(userData));
+    setUser(getLoggedUser());
+  }
+
+  function logout() {
+    localStorage.removeItem('userData');
+    setUser(null);
+  }
+
   return (
-    <Layout>
-      <Outlet />
-    </Layout>
+    <AuthContext.Provider value={{ user, login, logout }}>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </AuthContext.Provider>
   );
 }
 
