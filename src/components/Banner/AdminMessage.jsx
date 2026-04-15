61613ae136a0dc05855e1a5adc39cdfacac55de7
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-04-16 00:06:34 +0300

    Member able to make request to upgrade role

--- src/components/Banner/AdminMessage.jsx
+++ src/components/Banner/AdminMessage.jsx
@@ -1,14 +1,19 @@
-import { Link } from 'react-router';
-
+import { Link, useLocation } from 'react-router';
 function AdminMessage({ user }) {
+  const location = useLocation();
+  const isControlPanel = location.pathname === '/cp';
   return (
     <p className='text-center'>
       Welcome, <span className='font-bold'>{user.userName}</span> You're are logged in as an{' '}
       <span className='font-bold'>Admin</span>
-      <span className='mx-2'>•</span>
-      <Link to='/cp'>
-        <button className='text-tea_green-200 font-bold underline hover:cursor-pointer'>Control Panel</button>
-      </Link>
+      {!isControlPanel && (
+        <>
+          <span className='mx-2'>•</span>
+          <Link to='/cp'>
+            <button className='text-tea_green-200 font-bold underline hover:cursor-pointer'>Control Panel</button>
+          </Link>
+        </>
+      )}
     </p>
   );
 }
--- src/components/Banner/MemberMessage.jsx
+++ src/components/Banner/MemberMessage.jsx
@@ -1,11 +1,35 @@
 import Dialog from '../shared/Dialog';
 import { useState } from 'react';
-import { X as CloseIcon } from 'lucide-react';
+import { X as CloseIcon, LoaderCircle as LoadingIcon } from 'lucide-react';
+import { useNavigate } from 'react-router';
+import useFetch from '../../hooks/useFetch';
+import { useAuth } from '../../context/AuthContext';
 
 function MemberMessage({ user }) {
+  const navigate = useNavigate();
+  const { login } = useAuth();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const openDialog = () => setIsDialogOpen(true);
   const onClose = () => setIsDialogOpen(false);
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  };
+
+  const { triggerFetch, loading, error } = useFetch('/admin/request-role', { options, fetch: false });
+
+  async function handleConfirm() {
+    try {
+      const userData = await triggerFetch();
+      login(userData);
+      navigate(0);
+    } catch (err) {
+      // handled in the UI
+    }
+  }
 
   return (
     <div>
@@ -23,26 +47,29 @@ function MemberMessage({ user }) {
           position={'top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2'}
           dialogStatus={isDialogOpen}
           onClose={onClose}
+          disableEscKey={loading}
         >
           <div className='bg-papaya_whip-400 relative flex max-w-xl items-center gap-4 rounded-md p-4'>
-            <CloseIcon className='absolute top-2 right-4 hover:cursor-pointer' onClick={onClose} />
+            <CloseIcon
+              className='absolute top-2 right-4 hover:cursor-pointer disabled:hover:cursor-not-allowed'
+              onClick={onClose}
+              disabled={loading}
+            />
             <div className='space-y-2'>
               <h1 className='text-center font-bold'>Please Read!</h1>
+              {error && <p className='text-center text-red-600'>Something went wrong, please try again later!</p>}
               <p className='mb-4'>
                 This access is for testing purposes only. Please do not delete, edit, or post inappropriate content.
                 Help us keep the environment clean and functional for everyone.
               </p>
-              <div className='flex gap-2 font-bold'>
-                <button className='bg-tea_green-300 hover:bg-tea_green-200 w-full p-2 hover:cursor-pointer'>
-                  Confirm
-                </button>
-                <button
-                  className='bg-light_bronze-400 hover:bg-light_bronze-300 w-full p-2 hover:cursor-pointer'
-                  onClick={onClose}
-                >
-                  Cancel
-                </button>
-              </div>
+              <button
+                className='bg-tea_green-300 hover:bg-tea_green-200 flex w-full justify-center gap-4 p-2 hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500'
+                onClick={handleConfirm}
+                disabled={loading}
+              >
+                <p>Confirm</p>
+                {loading && <LoadingIcon className='animate-spin' />}
+              </button>
             </div>
           </div>
         </Dialog>
