0058e34d3ff040ca1df41a009285144dc3720497
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-05-06 11:34:16 +0300

    Update error component for reusability

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,11 @@
 import { Link, useNavigate, useRouteError } from 'react-router';
 import { useState, useEffect } from 'react';
-import Layout from './Layout';
 
-function Error() {
+function Error({ error }) {
   const [count, setCount] = useState(10);
   const navigate = useNavigate();
-  const error = useRouteError();
-  const message = error.status === 404 ? '404 Error - Page not found.' : 'Something went wrong';
+  const err = error ?? useRouteError();
+  const message = err.message ?? (err.status === 404 ? '404 Error - Page not found.' : 'Something went wrong');
 
   useEffect(() => {
     if (count <= 0) {
@@ -21,17 +20,15 @@ function Error() {
   }, [count]);
 
   return (
-    <Layout>
-      <div className='p-4'>
-        <div className='bg-papaya_whip-400 mt-12 space-y-2 rounded-md p-4 text-center'>
-          <h2>{message}</h2>
-          <p>You will be redirected to the homepage in {count} seconds.</p>
-          <Link to='/'>
-            <p className='font-bold'>Go to the homepage now</p>
-          </Link>
-        </div>
+    <div className='p-4'>
+      <div className='bg-papaya_whip-400 mt-12 space-y-2 rounded-md p-4 text-center'>
+        <h2>{message}</h2>
+        <p>You will be redirected to the homepage in {count} seconds.</p>
+        <Link to='/'>
+          <p className='font-bold'>Go to the homepage now</p>
+        </Link>
       </div>
-    </Layout>
+    </div>
   );
 }
 export default Error;
