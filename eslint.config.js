5d078da8863d0a25ff4a25ac90aa171dc881164c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-05-06 12:09:46 +0300

    TanStack query configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,14 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import query from '@tanstack/eslint-plugin-query';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
+      ...query.configs['flat/recommended'],
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from 'react-router';
 import Layout from './components/Layout';
 import { AuthProvider } from './context/AuthContext';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <AuthProvider>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
