4827f025e261770051dd7a1ba977d00f8c7b4007
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-05-06 13:51:51 +0300

    Refactor deleting to use TanStack query

--- src/components/shared/DeleteContent.jsx
+++ src/components/shared/DeleteContent.jsx
@@ -2,47 +2,47 @@ import { Trash2 as DeleteIcon } from 'lucide-react';
 import Dialog from './Dialog';
 import DialogWithConfirmBtn from './DialogWithConfirmBtn';
 import { useState } from 'react';
-import useFetch from '../../hooks/useFetch';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import fetchData from '../../helpers/fetchData';
 
-function DeleteContent({ contentId, route, user, onSuccess }) {
+// TODO: handle comment deletion
+
+function DeleteContent({ contentId, route, user }) {
+  const queryClient = useQueryClient();
+
   const keyword = route === '/admin/comments' ? 'comment' : 'post';
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const openDialog = () => setIsDialogOpen(true);
-  const onClose = () => {
-    setError(null);
-    setIsDialogOpen(false);
-  };
+  const onClose = () => setIsDialogOpen(false);
 
-  const { loading, error, setError, triggerFetch } = useFetch(route, { fetch: false });
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: ({ signal }) => {
+      const options = {
+        signal,
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          id: contentId,
+        }),
+      };
+      return fetchData(route, options);
+    },
 
-  async function handleConfirm() {
-    const options = {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${user.token}`,
-      },
-      body: JSON.stringify({
-        id: contentId,
-      }),
-    };
-
-    try {
-      setError(null);
-      await triggerFetch({ options });
-      onSuccess();
+    onSuccess: () => {
       onClose();
-    } catch (err) {
-      setError('Something went wrong... Try Again');
-    }
-  }
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+    },
+  });
 
   const dialogOptions = {
     onClose,
-    loading,
+    isPending,
     error,
-    handleConfirm,
+    mutate,
     title: 'Warning!',
     textContent: `Deleting this ${keyword} is permanent. Once deleted, it cannot be recovered. Continue?`,
   };
@@ -56,7 +56,7 @@ function DeleteContent({ contentId, route, user, onSuccess }) {
         position='top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2'
         dialogStatus={isDialogOpen}
         onClose={onClose}
-        disableEscKey={loading}
+        disableEscKey={isPending}
       >
         <DialogWithConfirmBtn options={dialogOptions} />
       </Dialog>
--- src/components/shared/DialogWithConfirmBtn.jsx
+++ src/components/shared/DialogWithConfirmBtn.jsx
@@ -4,7 +4,7 @@ function DialogWithConfirmBtn({ options }) {
   return (
     <div className='bg-papaya_whip-400 relative flex max-w-xl items-center gap-4 rounded-md p-4'>
       <button
-        disabled={options.loading}
+        disabled={options.isPending}
         className='absolute top-2 right-4 hover:cursor-pointer disabled:hover:cursor-not-allowed'
         onClick={options.onClose}
       >
@@ -16,11 +16,11 @@ function DialogWithConfirmBtn({ options }) {
         <p className='mb-4'>{options.textContent}</p>
         <button
           className='bg-tea_green-300 hover:bg-tea_green-200 flex w-full justify-center gap-4 p-2 hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500'
-          onClick={options.handleConfirm}
-          disabled={options.loading}
+          onClick={options.mutate}
+          disabled={options.isPending}
         >
           <p>Confirm</p>
-          {options.loading && <LoadingIcon className='animate-spin' />}
+          {options.isPending && <LoadingIcon className='animate-spin' />}
         </button>
       </div>
     </div>
