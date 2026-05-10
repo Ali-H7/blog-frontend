0971f492c1d1067e4edc963ea72441792ad0a3f4
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-05-10 13:48:27 +0300

    Refactor Tags component to use TanStack query

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,56 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router';
 import fetchData from '../../helpers/fetchData';
 import Skeleton from 'react-loading-skeleton';
 import RetryButton from '../shared/RetryButton';
 
 function Tags() {
-  const [tags, setTags] = useState([]);
-  const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState(null);
-  const [retryCount, setRetryCount] = useState(0);
+  const { data, isError, isPending, error, refetch } = useQuery({
+    queryKey: ['tags'],
+    queryFn: ({ signal }) => {
+      return fetchData(`/tags`, { signal });
+    },
+  });
 
-  function retry() {
-    setError(null);
-    setIsFetching(true);
-    setTimeout(() => setRetryCount((prev) => prev + 1), 500);
-  }
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    const options = { signal };
-
-    async function getTags() {
-      try {
-        const data = await fetchData('/tags', options);
-        setTags(data.tags);
-      } catch (err) {
-        if (err.name === 'AbortError') return;
-        setError(err.message);
-      } finally {
-        if (!signal.aborted) setIsFetching(false);
-      }
-    }
-    getTags();
-
-    return () => {
-      controller.abort();
-    };
-  }, [retryCount]);
-
-  const RetryProps = { error, retry };
   const parentClasses = 'flex flex-col items-center p-8 gap-2 min-w-full';
 
-  if (error) {
-    return (
-      <div className={parentClasses}>
-        <RetryButton {...RetryProps} />
-      </div>
-    );
-  }
+  if (isError) return <RetryButton error={error.message} retry={refetch} />;
 
-  if (isFetching) {
+  if (isPending) {
     return (
       <div className={parentClasses}>
         <Skeleton width={'5.5rem'} height={'2rem'} />
@@ -59,7 +25,7 @@ function Tags() {
     );
   }
 
-  if (!isFetching && tags.length === 0) {
+  if (!isPending && data?.tags?.length === 0) {
     return (
       <div className={parentClasses}>
         <p className='text-center'>No Tags Available</p>
@@ -69,9 +35,9 @@ function Tags() {
 
   return (
     <div className={parentClasses}>
-      <h1 className='text-2xl font-bold '>Tag List</h1>
-      <ul className='p-2 bg-tea_green-DEFAULT rounded-sm min-w-full flex flex-wrap justify-center gap-4'>
-        {tags.map((tag) => (
+      <h1 className='text-2xl font-bold'>Tag List</h1>
+      <ul className='bg-tea_green-DEFAULT flex min-w-full flex-wrap justify-center gap-4 rounded-sm p-2'>
+        {data?.tags?.map((tag) => (
           <li key={tag.id} className='border-b border-transparent hover:border-black'>
             <Link to={`/tags/${tag.slug}`}>
               {tag.name}
